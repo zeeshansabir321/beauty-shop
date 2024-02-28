87bb81572efdb35cb4ca56d11c3572317738387e
zeeshansabir321 <zeeshansabir321@outlook.com>
2024-02-28 06:52:25 -0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fff8f0",
+        secondary: "#fec377",
+        complementary: "#fec377",
+        body: "#ffffff",
+      }
+    },
+  },
+  plugins: [],
+}
+
