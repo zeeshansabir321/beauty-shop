abc3113a80e4c478a235051b37762e74ef15fba9
zeeshansabir321 <zeeshansabir321@outlook.com>
2024-02-28 23:52:39 -0800

    Write header and hero section code only for mobile for now and add custom css file by name custom.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,15 @@ module.exports = {
       colors: {
         primary: "#fff8f0",
         secondary: "#fec377",
-        complementary: "#fec377",
+        complementary: "#fd9f2d",
+        green : "#569310",
         body: "#ffffff",
-      }
+      },
+    
+      zIndex: {
+        "minus": "-1"
+      },
+
     },
   },
   plugins: [],
